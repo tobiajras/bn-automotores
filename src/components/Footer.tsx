615fb5f7ac7ab5083c8341b57004acccb8bd948f
tobiajras <tobyajras@gmail.com>
2025-08-31 16:54:25 -0300

    whatsapp icon removed

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,6 @@ import { company } from '@/app/constants/constants';
 import Image from 'next/image';
 import InstagramIcon from './icons/InstagramIcon';
 import FacebookIcon from './icons/FacebookIcon';
-import WhatsappIcon from './icons/WhatsappIcon';
 
 import Link from 'next/link';
 import GravityLogo from './icons/GravityLogo';
