3e7ee2121054ce00491cc87606cd216603d0ef0b
tobiajras <tobyajras@gmail.com>
2025-11-06 16:44:32 -0300

    bn automotores tandil

--- src/app/constants/constants.tsx
+++ src/app/constants/constants.tsx
@@ -29,7 +29,7 @@ export const API_BASE_URL = 'https://only-motors.agenciagrvity.com';
 export const TENANT = 'pozner';
 
 export const metadataCompany = {
-  metadataBase: 'https://bnautomotores.vercel.app/',
+  metadataBase: 'https://bnautomotorestandil.vercel.app/',
   title: 'BN Automotores - Compra y Venta de Autos',
   description:
     'Compra y Venta de Autos: Vehículos seleccionados, atención personalizada.',
